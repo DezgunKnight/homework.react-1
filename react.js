a582a0301cfd25d729cecfb8d04647c7485f6521
DezgunKnight <berelidzevaja11@gmail.com>
2022-08-07 17:41:52 +0400

    4 exercises done except 3

---
+++ react.js
@@ -0,0 +1,36 @@
+// class course {
+//     constructor(title, mentorName, numOfStudents, price) {
+//         this.title = title
+//         this.mentorName = mentorName
+//         this.numOfStudents = numOfStudents
+//         this.price = price
+//     }
+
+// }
+
+// const c = new course('react', 'Vano', 15, 1200);
+
+// console.log(c);
+
+
+// let user = {
+//     name: "Vaniko",
+//     skills: ["React", "Node"]
+//   };
+// let {name:firstName, skills:stack} = user;
+// console.log(firstName, stack);
+
+
+
+// const person1 = {
+//     name: "Giorgi",
+//     age: 40,
+//   };
+
+//   const person2 = person1;
+
+//   person2.name = "Gvantsa";
+
+//   console.log(person1); 
+//   console.log(person2); 
+// am davalebashi imitom sheicvala orive personis saxeli rom chven pirveli cvladis informacia mtlianad shevinaxet sxva cvladshi da person 1 avtomaturad gaxda person 2is shvili xoda rogorc mshoblebs undat egre iqnebao da person 1is info shecvala radgan es info gaxda avtomaturad person 2is infoc.
